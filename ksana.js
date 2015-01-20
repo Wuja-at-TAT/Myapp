32f1b4ff34e6cb4ace5f9a05670c69cbcf71659b
Richard <richard@anser-u2.com>
2015-01-20 20:41:36 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,42 @@
+jsonp_handler({
+ "version": "1",
+ "build": 5,
+ "title": "Myapp",
+ "minruntime": 1.3,
+ "baseurl": "http://rawgit.com/Wuja-at-TAT/Myapp/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  552,
+  64,
+  155352,
+  277980,
+  645243,
+  765,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-01-20T12:31:12.000Z",
+  "2015-01-20T12:31:12.000Z",
+  "2015-01-20T12:37:18.000Z",
+  "2015-01-20T12:31:12.000Z",
+  "2015-01-20T12:31:12.000Z",
+  "2015-01-20T12:35:28.000Z",
+  "2015-01-20T12:31:12.000Z",
+  "2015-01-20T12:31:12.000Z",
+  "2015-01-20T12:32:31.000Z"
+ ],
+ "date": "2015-01-20T12:37:19.690Z"
+})
